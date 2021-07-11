a94838774eeac342f20439fd089e7bbdf8cabe4d
Harshit Yadav <harshit.yd.47@gmail.com>
2021-07-12 01:01:50 +0530

    added login fucntionality and made pages for creating blog, adding photos, etc.

--- index.js
+++ index.js
@@ -3,87 +3,186 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 // const session = require("express-session");
 // const passport = require("passport");
 
 const app = express();
-
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+let token = "";
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.createConnection("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.createConnection("mongodb://localhost:27017/adminDB", {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.set("useCreateIndex", true);
 
+//------------------- bcrypt passwords ------------------------//
+
+let admin = { mahima: "",
+            tushita: ""
+};
+
+const pws = process.env.pw.split(",");
+
+bcrypt.hash(pws[0], parseInt(process.env.SALT_ROUNDS), function (err, hash) {
+    admin.mahima = hash;
+});
+
+bcrypt.hash(pws[1], process.env.SALT_ROUNDS, function (err, hash) {
+    admin.tushita = hash;
+});
+
+
+
+//------------------- ---------------- ------------------------//
 
 //------------------- blog ------------------------//
 
-const postSchema = {
+mongoose.createConnection("mongodb://localhost:27017/familyDentalDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    message: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+const postSchema = new mongoose.Schema({
     title: String,
     topic: String,
-    originDate: {type: Date, default: Date.now},
+    originDate: { type: Date, default: Date.now },
     content: String
-  };
+});
 
 const Post = mongoose.model("Post", postSchema);
+
+const imageSchema = new mongoose.Schema({
+    link: String,
+    desc: String
+});
+
+const Image = mongoose.model("Image", imageSchema);
+
+
 //------------------- ---- ------------------------//
 
-app.get("/", function(req, res){
-    res.render("home", {MAP_KEY: process.env.MAP_KEY});
+//------------------- blog form input ------------------------//
+
+app.get("/addBlog", function (req, res) {
+    res.render("addBlog");
 });
 
-app.get("/home", function(req, res){
-    res.render("home", {MAP_KEY: process.env.MAP_KEY});
+app.post("/addBlog", function (req, res) {
+    const post = new Post({
+        title: req.body.postTitle,
+        topic: req.body.postTopic,
+        content: req.body.postBody
+    });
+    post.save(function (err) {
+        if (!err) {
+            res.redirect("/");
+        }
+    });
+});
+//------------------------ ---- ------------------------------//
+
+//------------------- backdoor-Dashboard ------------------------//
+
+// app.get("/backdoor", function(req, res){
+
+// });
+
+//------------------------ ---- ------------------------------//
+app.get("/", function (req, res) {
+    res.render("home");
 });
 
-app.get("/ourClinic", function(req,res){
-    res.render("ourClinic", {MAP_KEY: process.env.MAP_KEY});
+app.get("/home", function (req, res) {
+    res.render("home");
 });
 
-app.get("/ourDoctors", function(req,res){
-    res.render("ourDoctors", {MAP_KEY: process.env.MAP_KEY});
+app.get("/ourClinic", function (req, res) {
+    res.render("ourClinic");
 });
 
-app.get("/implants", function(req,res){
-    res.render("implants", {MAP_KEY: process.env.MAP_KEY});
+app.get("/ourDoctors", function (req, res) {
+    res.render("ourDoctors");
 });
 
-app.get("/admin", function(req,res){
-    res.render("admin");
-});
-
-app.get("/blog", function(req,res){
-    res.render("blog");
-});
-
-
-
-app.get("/backdoor", function(req,res){
-    res.render("backdoor");
-});
-
-
-app.post("/admin", function(req,res){
-
-});
-
-app.get("/login", function(req,res){
-    res.render("login");
-});
-
-app.get("/invisalign", function(req, res){
+app.get("/invisalign", function (req, res) {
     res.render("invisalign");
 });
 
-app.get("/treatments", function(req, res){
+app.get("/implants", function (req, res) {
+    res.render("implants");
+});
+
+app.get("/treatments", function (req, res) {
     res.render("treatments");
 });
 
-app.get("/backdoor", function(req,res){
-    res.render("backdoor");
+app.get("/blog", function (req, res) {
+    // Post.find({}, function(err, posts){
+    //     res.render("blog", {
+    //       posts: posts
+    //       });
+    //   });
 });
 
-app.listen(3000, function(){
+app.get("/admin", function (req, res) {
+    res.render("admin");
+});
+
+app.post("/admin", function (req, res) {
+    const username = req.body.user_name;
+    if(admin.hasOwnProperty(username)){
+        bcrypt.compare(req.body.pass, admin[username], function (err, result) {
+            // console.log({username, result});
+            if(result){
+                token = jwt.sign({
+                    user_name: username
+                }, process.env.JWT_KEY,
+                {
+                    expiresIn: "2h"
+                });
+                console.log(token);
+                res.redirect("/backdoor");
+            }
+            else {
+                console.log("wrong password");
+                res.redirect("/admin");
+            }
+            
+        });
+    }
+    else{
+        console.log("Wrong username");
+        res.redirect('/admin');
+    }
+});
+
+console.log(token);
+
+app.get("/backdoor", function (req, res) {
+    try{
+        jwt.verify(token, process.env.JWT_KEY, function(err,  decoded){
+            console.log({decoded, err});
+            if(!err){
+                res.redirect("/backdoor");
+            }
+            else{
+                res.redirect("/admin");
+            }
+        }); 
+    }
+    catch(error){
+        res.redirect("/admin");
+    }
+});             
+
+app.listen(3000, function () {
     console.log("Server is running at port 3000.");
 });
